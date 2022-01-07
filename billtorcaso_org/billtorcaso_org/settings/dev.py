e138ab84a30b2121462d25cbef98dc7b5f4a5824
Ubuntu@BillTorcaso <torcasobill@gmail.com>
2022-01-07 17:08:13 +0000

    These files are the product of 'wagtail start billtorcaso_org.

---
+++ billtorcaso_org/billtorcaso_org/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-26&bygu#eqdym8fdyb8zg3cfnwl4xm@_2n1t_v_8+p5_+^23%@'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
